66fc17cbf586c4e68917ce0697eb548c0c98d9e7
mirshifatmahmud25 <mahmud15-4857@diu.edu.bd>
2025-06-25 17:38:58 +0600

    api.php modify

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,7 @@ use App\Http\Controllers\Payment\PaymentController;
 use App\Http\Controllers\QuotesController;
 use App\Http\Controllers\ReferralController;
 use App\Http\Controllers\SendEnquiryController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 // public route
 Route::post('/register', [AuthController::class, 'register']);
@@ -20,37 +15,18 @@ Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 Route::post('/resend-otp', [AuthController::class, 'resendOtp']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-
-// check zip code
 Route::post('/check-zip-code', [CheckZipCodeController::class, 'checkZipCode']);
-
-// private route with jwt auth
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/change-password', [AuthController::class, 'changePassword']);
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/update-profile', [AuthController::class, 'updateProfile']);
     Route::post('/update-password', [AuthController::class, 'updatePassword']);
-
-    // Route::post('/avatar', [AuthController::class, 'avatar']);
-    // Route::post('/update-avatar', [AuthController::class, 'updateAvatar']);
-
-    // All referrals data showing
     Route::get('/all-referred-info', [ReferralController::class, 'allReferredUsers']);
-
-
-
-    // send enquiry (mail send to admin)
     Route::post('/send-enquiry', [SendEnquiryController::class, 'sendEnquiry']);
-
-    // discount $ monthly charge
     Route::get('/discount-charge', [GetDiscountChargeController::class, 'getDiscountCharge']);
     Route::get('/quote', [QuotesController::class, 'quote']);
-
-    // create payment intent
     Route::post('/payment-intent', [PaymentController::class, 'paymentIntent']);
     Route::post('/payment-success', [PaymentController::class, 'paymentSuccess']);
-
-    // get previous history
     Route::get('/get-previous-history', [PaymentController::class, 'getPreviousHistory']);
 });
