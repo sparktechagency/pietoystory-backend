c95aa20e9ed28902389da9c315ec179c672b8d5e
Mir Shifat Mahmud <mir19438.bdcallingit@gmail.com>
2025-10-24 12:15:06 +0600

    send enquiry and payment success client gmail clearpathpetwasteinfo@gmail.com added.

--- app/Http/Controllers/SendEnquiryController.php
+++ app/Http/Controllers/SendEnquiryController.php
@@ -38,7 +38,7 @@ class SendEnquiryController extends Controller
 
 
         try {
-            Mail::to('clearpathpetwasteinfo@gmail.com')->send(new SendEnquiryMail($data));
+            Mail::to('shifatghi@gmail.com')->send(new SendEnquiryMail($data));
         } catch (Exception $e) {
             Log::error($e->getMessage());
         }
